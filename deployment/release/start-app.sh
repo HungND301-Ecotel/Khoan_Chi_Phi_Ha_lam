354814b9f1cd9a21a7769f100ef06116d8ac4590
ptchd03 <congpham03hd@gmail.com>
2026-03-18 16:21:40 +0700

    fix: fix start app

--- deployment/release/start-app.sh
+++ deployment/release/start-app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Start the application in the staging environment
+# Start the application in the release environment
 # Docker compose for deployment
-docker compose -f staging-docker-compose.yaml down # always down first
-docker compose -f staging-docker-compose.yaml pull # always pull the latest images
+docker compose -f release-docker-compose.yaml down # always down first
+docker compose -f release-docker-compose.yaml pull # always pull the latest images
 # run containers in detached mode
-nohup docker compose -f staging-docker-compose.yaml up  -d > deploy.log 2>&1 & 
+nohup docker compose -f release-docker-compose.yaml up  -d > deploy.log 2>&1 & 
 # Check built images
 docker image prune -f # clean up unused images
 docker images
