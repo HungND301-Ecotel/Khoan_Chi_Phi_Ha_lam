0e3b34cd45accb0003af2f2d50e7cd8545ad5aa6
chienminh123 <chien08042k4@gmail.com>
2026-06-22 17:21:02 +0700

    fix

--- backend/Ecotel.KCPCMS.BE/src/Infrastructures/Migrators/Migrators.Postgres/Migrations/20260622071020_UpdateNormFactor.cs
+++ backend/Ecotel.KCPCMS.BE/src/Infrastructures/Migrators/Migrators.Postgres/Migrations/20260622071020_UpdateNormFactor.cs
@@ -56,6 +56,17 @@ namespace Migrators.PostgreSQL.Migrations
                 schema: "Index",
                 table: "NormFactorAssignmentCode");
 
+            migrationBuilder.Sql(@"
+        DELETE FROM ""Index"".""NormFactorAssignmentCode""
+        WHERE ""Id"" NOT IN (
+            SELECT DISTINCT ON (""NormFactorId"", ""AssignmentCodeId"") ""Id""
+            FROM ""Index"".""NormFactorAssignmentCode""
+            WHERE ""DeletedOn"" IS NULL
+            ORDER BY ""NormFactorId"", ""AssignmentCodeId"", ""CreatedOn"" DESC
+        )
+        AND ""DeletedOn"" IS NULL;
+    ");
+
             migrationBuilder.DropForeignKey(
                 name: "FK_NormFactorAssignmentCode_Material_MaterialId",
                 schema: "Index",
